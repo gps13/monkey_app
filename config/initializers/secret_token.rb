1a04a9bbf8d153df1dd6841a3f94a54b0cc4038c
gps13 <g.psistakis@gmail.com>
2013-06-27 11:12:57 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monkey::Application.config.secret_token = '482aeb6de714491cc8a968f93c940e7ba0019065010c65fbfe0cad0035cb2d5c52743efc053995763f1da005f2b7d0a5e2d3998f0368eb2f924e2802e398a387'
